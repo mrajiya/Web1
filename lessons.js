340bff2a577d9a81b02a7d8fbc97d983594323c9
mrajiya <93322893+mrajiya@users.noreply.github.com>
2021-11-19 16:25:53 +0400

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,110 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "CHEMISTRY",
+        "location": "NIGERIA",
+        "price": 3000,
+        "image": "images/chemistry.jpg",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 1002,
+        "title": "MATHs",
+        "location": "JAPAN",
+        "price": 1000,
+        "image": "images/maths.jpg",
+        "availableInventory": 4,
+        "rating": 3.5
+    },
+    {
+        "id": 1003,
+        "title": "ENGINEERING",
+        "location": "USA",
+        "price": 5000,
+        "image": "images/civil.jpg",
+        "availableInventory": 3,
+        "rating": 5
+    },
+    {
+        "id": 1004,
+        "title": "ENGLISH",
+        "location": "NIGERIA",
+        "price": 1000,
+        "image": "images/english.jpg",
+        "availableInventory": 4,
+        "rating": 3
+    },
+    {
+        "id": 1005,
+        "title": "SPORTS",
+        "location": "SAUDI",
+        "price": 3000,
+        "image": "images/sports.jpg",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1006,
+        "title": "LAW",
+        "location": "JAPAN",
+        "price": 1500,
+        "image": "images/law.jpg",
+        "availableInventory": 5,
+        "rating": 3
+    },
+    {
+        "id": 1007,
+        "title": "PHYSICS",
+        "location": "JORDAN",
+        "price": 2000,
+        "image": "images/physics.jpg",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1008,
+        "title": "MECHANICS",
+        "location": "FRANCE",
+        "price": 3500,
+        "image": "images/mechanics.jpg",
+        "availableInventory": 3,
+        "rating": 5
+    },
+    {
+        "id": 1009,
+        "title": "HISTORY",
+        "location": "CHINA",
+        "price": 500,
+        "image": "images/history.jpg",
+        "availableInventory": 5,
+        "rating": 1
+    },
+    {
+        "id": 1010,
+        "title": "WEB APP",
+        "location": "TOGO",
+        "price": 5000,
+        "image": "images/webapp.jpg",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id": 1011,
+        "title": "BIOLOGY",
+        "location": "LONDON",
+        "price": 2500,
+        "image": "images/biology.jpg",
+        "availableInventory": 3,
+        "rating": 2
+    },
+    {
+        "id": 1012,
+        "title": "SCIENCE",
+        "location": "NIGERIA",
+        "price": 5000,
+        "image": "images/science.jpg",
+        "availableInventory": 5,
+        "rating": 5
+    }
+]
